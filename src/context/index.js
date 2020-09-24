4fc387d32a49608ad66932c218c6ebff60611951
ColoMiranda <marcos.miranda.1995@gmail.com>
2020-09-24 19:42:16 -0300

    'master'

---
+++ src/context/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { 
+    ProjectsContext,
+    ProjectsProvider,
+    useProjectsValue
+} from './projects-context';
+
+import {
+    SelectedProjectContext,
+    SelectedProjectProvider,
+    useSelectedProjectValue
+} from './selected-projects-context';
+
+export { 
+    ProjectsContext,
+    ProjectsProvider,
+    useProjectsValue,
+    SelectedProjectContext,
+    SelectedProjectProvider,
+    useSelectedProjectValue
+};
+
